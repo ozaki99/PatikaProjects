16cfe8fcc5465a232c344fc912da30560e1f06b5
ozaki99 <m.ozturk1251@gmail.com>
2023-05-02 02:25:51 +0300

    BMI hesaplama eklendi

---
+++ BodyMassIndex.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+
+        /*
+        Vücut Kitle İndeksi Hesaplama
+        Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
+        Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+
+        Formül
+        Kilo (kg) / Boy(m) * Boy(m)
+        */
+
+        int weight;
+        double lenght;
+        double bmi;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen kilonuzu giriniz (KG): ");
+        weight = input.nextInt();
+
+        System.out.print("Lütfen boyunuzu girin (M,CM): ");
+        lenght = input.nextDouble();
+
+        bmi = (weight / (lenght * lenght));
+
+        System.out.println("BMI Hesaplandı: " + bmi);
+
+    }
+}
