5e4834700c40706bf67c2c0f41c199fdbceccfb6
ozaki99 <m.ozturk1251@gmail.com>
2023-05-10 15:34:56 +0300

    Örüntü oluşturma sistemi geliştirildi ve düzenlendi by ozaki99

--- DeseneGoreMetot.java
+++ DeseneGoreMetot.java
@@ -1,14 +1,15 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class DeseneGoreMetot {
-    static int metot(int a, int x) {
+    static int metot(int a, int oruntu, int x) {
         if (a > 0) {
             System.out.print(a + " ");
-            return metot(a - 5, x);
+            return metot(a - oruntu, oruntu, x);
         } else {
             System.out.print(a + " ");
             while (a != x) {
-                System.out.print((a += 5) + " ");
+                System.out.print((a += oruntu) + " ");
             }
         }
         return a;
@@ -16,8 +17,34 @@ public class DeseneGoreMetot {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("BIR SAYI GIRIN: ");
-        int sayi = input.nextInt();
-        metot(sayi, sayi);
+        int answer;
+        do {
+            System.out.println("\nORUNTU OLUSTURMA PROGRAMINA HOSGELDINIZ\n" +
+                    "1- PROGRAMI CALISTIR\n" +
+                    "2- NASIL CALISIR\n" +
+                    "3- CIKIS");
+            System.out.print("\n--> : ");
+            answer = input.nextInt();
+
+            if (answer == 1){
+                System.out.print("BIR SAYI GIRIN: ");
+                int sayi = input.nextInt();
+                System.out.print("ORUNTU SAYISI GIRIN: ");
+                int oruntu = input.nextInt();
+                metot(sayi, oruntu, sayi);
+            }
+            if (answer == 2){
+                System.out.println("ILK ONCE BIR SAYI GIRIN ARDINDAN KACAR KACAR \n" +
+                        "AZALIP ARTACAGINI BELIRLEYEREK BIR ORUNTU OLUSTURUN.\n");
+            }
+            if (answer == 3){
+                System.out.println("HOSCAKALIN...");
+                break;
+            }
+            if (answer > 3){
+                System.out.println("HATALI GIRIS YAPTINIZ!!!");
+            }
+        } while (answer != 3);
+
     }
 }
